94c313684fe981b3f36019b516e14f9511dcc2f9
John W. Ellis <jellis@yieldbot.com>
2017-04-17 16:34:59 -0400

    mock the yieldbot intent tag library functions used; use the mock object

--- test/spec/adapters/yieldbot_spec.js
+++ test/spec/adapters/yieldbot_spec.js
@@ -1,293 +1,143 @@
 import {expect} from 'chai';
 import {assert} from 'chai';
-import Adapter from '../../../src/adapters/yieldbot';
+import YieldbotAdapter from '../../../src/adapters/yieldbot';
 import bidManager from '../../../src/bidmanager';
 import adLoader from '../../../src/adloader';
 
-describe('yieldbot adapter tests', () => {
-    let adapter;
-    let sandbox;
-    let yieldbotLibStub;
-    let YldbotLoadScript;
-    let bidsRequestBuff;
-    const bidderRequest = {
-        bidderCode: 'yieldbot',
-        bidder: 'yieldbot',
-        bidderRequestId: '187a340cb9ccc5',
-        bids: [
-                {
-                    bidId: '2640ad280208cc',
-                    sizes: [[300, 250], [300, 600]],
-                    bidder: 'yieldbot',
-                    bidderRequestId: '187a340cb9ccc5',
-                    params: { psn: '1234', slot: 'medrec' },
-                    requestId: '5f297a1f-3163-46c2-854f-b55fd2e74ece',
-                    placementCode: 'div-gpt-ad-1460505748561-0'
-                    },
-                    {
-                    bidId: '35751f10be5b6b',
-                    sizes: [[728, 90], [970, 90]],
-                    bidder: 'yieldbot',
-                    bidderRequestId: '187a340cb9ccc5',
-                    params: { psn: '1234', slot: 'leaderboard' },
-                    requestId: '5f297a1f-3163-46c2-854f-b55fd2e74ece',
-                    placementCode: 'div-gpt-ad-1460505661639-0'
-                    }
+let adapter;
+let sandbox;
+let yieldbotLibStub;
+let YldbotLoadScript;
 
-        ]
-    };
-
-  const YB_BID_FIXTURE = {
-    medrec: {
-      ybot_ad: 'y',
-      ybot_slot: 'medrec',
-      ybot_cpm: '200',
-      ybot_size: '300x250'
+const bidderRequest = {
+  bidderCode: 'yieldbot',
+  bidder: 'yieldbot',
+  bidderRequestId: '187a340cb9ccc5',
+  bids: [
+    {
+      bidId: '2640ad280208cc',
+      sizes: [[300, 250], [300, 600]],
+      bidder: 'yieldbot',
+      bidderRequestId: '187a340cb9ccc5',
+      params: { psn: '1234', slot: 'medrec' },
+      requestId: '5f297a1f-3163-46c2-854f-b55fd2e74ece',
+      placementCode: 'div-gpt-ad-1460505748561-0'
     },
-    leaderboard: {
-      ybot_ad: 'y',
-      ybot_slot: 'leaderboard',
-      ybot_cpm: '100',
-      ybot_size: '728x90'
+    {
+      bidId: '35751f10be5b6b',
+      sizes: [[728, 90], [970, 90]],
+      bidder: 'yieldbot',
+      bidderRequestId: '187a340cb9ccc5',
+      params: { psn: '1234', slot: 'leaderboard' },
+      requestId: '5f297a1f-3163-46c2-854f-b55fd2e74ece',
+      placementCode: 'div-gpt-ad-1460505661639-0'
+    }
+  ]
+};
+
+const YB_BID_FIXTURE = {
+  medrec: {
+    ybot_ad: 'y',
+    ybot_slot: 'medrec',
+    ybot_cpm: '200',
+    ybot_size: '300x250'
+  },
+  leaderboard: {
+    ybot_ad: 'y',
+    ybot_slot: 'leaderboard',
+    ybot_cpm: '100',
+    ybot_size: '728x90'
+  }
+};
+
+
+function createYieldbotMockLib() {
+  window.yieldbot = {
+    _initialized: false,
+    pub: (psn) => {},
+    defineSlot: (slotName, optionalDomIdOrConfigObject, optionalTime) => {},
+    enableAsync: () => {},
+    go: () => { window.yieldbot._initialized = true; },
+    nextPageview: (slots, callback) => {},
+    updateState: (data) => {
+    },
+    getSlotCriteria: (slotName) => {
+      return YB_BID_FIXTURE[slotName] || {ybot_ad: "n"};
     }
   };
+}
 
+function restoreYieldbotMockLib() {
+  window.yieldbot = null;
+}
 
-  function createYieldbotMockLib() {
-    window.yieldbot = {
-      _initialized: false,
-      pub: (psn) => {},
-      defineSlot: (slotName, optionalDomIdOrConfigObject, optionalTime) => {},
-      enableAsync: () => {},
-      go: () => { window.yieldbot._initialized = true; },
-      nextPageview: (slots, callback) => {},
-      getSlotCriteria: (slotName) => {
-        return YB_BID_FIXTURE[slotName] || {ybot_ad: "n"};
-      }
-    };
-  }
+function mockLoadAndBidRequest() {
+  window.ybotq = window.ybotq || [];
+  window.ybotq.forEach(fn => {
+    fn.apply(window.yieldbot);
+  });
+  window.ybotq = [];
+}
 
-  function restoreYieldbotMockLib() {
-    window.yieldbot = null;
-  }
-  beforeEach(() => {
-    adapter = new Adapter();
+before(function() {
+  pbjs._bidsRequested.push(bidderRequest);
+});
+
+describe('Yieldbot adapter tests', function() {
+
+  beforeEach(function() {
+    adapter = new YieldbotAdapter();
     sandbox = sinon.sandbox.create();
   });
 
-  afterEach(() => {
+  afterEach(function() {
     sandbox.restore();
   });
 
-  describe('callBids', () => {
-    beforeEach(() => {
+  let yieldbotStub;
+  describe('callBids', function() {
+    beforeEach(function () {
       createYieldbotMockLib();
       sandbox.stub(adLoader, 'loadScript');
-      pbjs._bidsRequested.push(bidderRequest);
+      yieldbotStub = sandbox.stub(window.yieldbot);
     });
 
     afterEach(() => {
       restoreYieldbotMockLib();
-      //pbjs._bidsRequested = [];
     });
 
-    it('should be called once', () => {
+    it('should request the yieldbot library', function() {
       adapter.callBids(bidderRequest);
-      ybotq[0]();
-      ybotq[1]();
+      mockLoadAndBidRequest();
       sinon.assert.calledOnce(adLoader.loadScript);
+      sinon.assert.calledWith(adLoader.loadScript, '//cdn.yldbt.com/js/yieldbot.intent.js');
     });
 
+    it('should set a yieldbot psn', function() {
+      adapter.callBids(bidderRequest);
+      mockLoadAndBidRequest();
+      sinon.assert.called(yieldbotStub.pub);
+      sinon.assert.calledWith(yieldbotStub.pub, '1234');
+    });
+
+    it('should define yieldbot slots', function() {
+      adapter.callBids(bidderRequest);
+      mockLoadAndBidRequest();
+      sinon.assert.calledTwice(yieldbotStub.defineSlot);
+      sinon.assert.calledWith(yieldbotStub.defineSlot, 'medrec', {sizes: [[300, 250], [300, 600]]});
+      sinon.assert.calledWith(yieldbotStub.defineSlot, 'leaderboard', {sizes: [[728, 90], [970, 90]]});
+    });
+
+    it('should enable yieldbot async mode', function() {
+      adapter.callBids(bidderRequest);
+      mockLoadAndBidRequest();
+      sinon.assert.called(yieldbotStub.enableAsync);
+    });
+
+    it('should handle yieldbot updateState callback', function() {
+      adapter.callBids(bidderRequest);
+      mockLoadAndBidRequest();
+      sinon.assert.calledOnce(yieldbotStub.updateState);
+    });
   });
 });
-
- // Working on the below tests to set the URL
-
-    /*it("sets url parameters", function () {
-
-        adapter.callBids(bidderRequest);
-
-        var bidUrl = YldbotLoadScript.getCall(0).args[0];
-        var parsedBidUrl = urlParse(bidUrl);
-        var parsedBidUrlQueryString = querystringify.parse(parsedBidUrl.query);
-
-        expect(parsedBidUrlQueryString.hostname).to.equal('cdn.yldbt.com');
-        expect(parsedBidUrlQueryString.pathname).to.equal('/js/yieldbot.intent.js');
-
-           var bidObjArr = JSON.parse(parsedBidUrlQueryString);
-
-            expect(bidObjArr).to.have.property('bids');
-            var bidObj1 = bidObjArr.bids[0];
-
-            expect(bidObj1.sizes).to.have.property('sizes').and.to.equal('300x250');
-            expect(bidObj1.psn).to.have.property('psn').and.to.equal('1234');
-            expect(bidObj1.psn).to.have.property('placementCode').and.to.equal('div-gpt-ad-1460505748561-0');
-
-
-            var bidObj2 = bidObjArr.bids[1];
-
-            expect(bidObj2).to.have.property('sizes').and.to.equal('728x90');
-            expect(bidObj2).to.have.property('psn').and.to.equal('1234');
-            expect(bidObj1.psn).to.have.property('placementCode').and.to.equal('div-gpt-ad-1460505661639-0');
-    });
-});
-
-/*describe('yieldbot adapter tests', function () {
-
-    var expect = require('chai').expect;
-    var assert = require('chai').assert;
-    var urlParse = require('url-parse');
-    var querystringify = require('querystringify');
-
-    window.pbjs = window.pbjs || {};
-    if (typeof(ybotq)==="undefined"){
-        var ybotq = window.ybotq;
-    }
-
-    let adapter;
-	let sandbox;
-
-	beforeEach( function() {
-        adapter = new Adapter();
-		sandbox = sinon.sandbox.create();
-		sandbox.stub(adLoader, 'loadScript');
-	});
-
-	afterEach( function() {
-		sandbox.restore();
-	});
-
- describe('creation of bid url', function () {
-        it('should be called once', function () {
-
-            var bidderRequest = {
-                bidderCode: 'yieldbot',
-                bidder: 'yieldbot',
-                bids: [
-                        {
-		          bidId: '202c119164171c',
-     		          sizes: [[300, 250], [300, 600]],
-                          bidder: 'yieldbot',
-                          params: { psn: '1234', slot: 'medrec' },
-                          requestId: '35f5139c-dddb-46d4-8d57-bc21bab366f4',
-                          placementCode: 'div-gpt-ad-1460505661639-0'
-                         }
-
-                ]
-            };
-
-            adapter.callBids(bidderRequest);
-
-        });
-
-        it('should have load the script', function () {
-            var url = void 0;
-            url = adLoader.loadScript;
-            expect(url).to.equal('//cdn.yldbt.com/js/yieldbot.intent.js');
-            console.log('check my URL:', url);
-
-        });
-
-       it('should fix parameter name', function () {
-
-            var bidderRequest = {
-                bidderCode: 'yieldbot',
-                bidder: 'yieldbot',
-                bids: [
-                        {
-                            bidId: '2155fc9a2db648',
-                            sizes: [[300, 250], [300, 600]],
-                            bidder: 'yieldbot',
-                            params: { psn: '1234', slot: 'medrec' },
-                            requestId: '6d6802a-e3d9-49bf-b0eb-2e5e249ae54b',
-                            placementCode: 'div-gpt-ad-1460505748561-0'
-                        }
-
-                ]
-            };
-
-            adapter.callBids(bidderRequest);
-
-        var requestURI =  adLoader.loadScript.getCall(0).args[0];
-
-        sinon.assert.calledWith( adLoader.loadScript, requestURI);
-        console.log('what:',  adLoader.loadScript);
-
-        var parsedBidUrl = urlParse(requestURI);
-        var parsedBidUrlQueryString = querystringify.parse(parsedBidUrl.query);
-        console.log('what is in this:', parsedBidUrlQueryString);
-
-        expect(parsedBidUrl.hostname).to.equal('cdn.yldbt.com');
-        expect(parsedBidUrl.pathname).to.equal('/js/yieldbot.intent.js');
-
-        expect(parsedBidUrlQueryString).to.have.property('sizes').and.to.equal('300x250');
-        expect(parsedBidUrlQueryString).to.have.property('bidId').and.to.equal('2155fc9a2db648');
-        expect(parsedBidUrlQueryString).to.have.property('placementCode').and.to.equal('div-gpt-ad-1460505748561-0');
-
-        });
-
-     });
-
-   describe('placement by size', function () {
-
-       it('should be called with specific parameters for two bids', function () {
-
-            var bidderRequest = {
-                bidderCode: 'yieldbot',
-                bidder: 'yieldbot',
-                bidderRequestId: '187a340cb9ccc5',
-                bids: [
-                        {
-                            bidId: '2640ad280208cc',
-                            sizes: [[300, 250], [300, 600]],
-                            bidder: 'yieldbot',
-                            bidderRequestId: '187a340cb9ccc5',
-                            params: { psn: '1234', slot: 'medrec' },
-                            requestId: '5f297a1f-3163-46c2-854f-b55fd2e74ece',
-                            placementCode: 'div-gpt-ad-1460505748561-0'
-                            },
-                            {
-                            bidId: '35751f10be5b6b',
-                            sizes: [[728, 90], [970, 90]],
-                            bidder: 'yieldbot',
-                            bidderRequestId: '187a340cb9ccc5',
-                            params: { psn: '1234', slot: 'leaderboard' },
-                            requestId: '5f297a1f-3163-46c2-854f-b55fd2e74ece',
-                            placementCode: 'div-gpt-ad-1460505661639-0'
-                            }
-
-                ]
-            };
-
-            adapter.callBids(bidderRequest);
-
-            var bidUrl = ybotLoadScript.getCall(0).args[0];
-
-            sinon.assert.calledWith(ybotLoadScript, bidUrl);
-
-            var parsedBidUrl = urlParse(bidUrl);
-            var parsedBidUrlQueryString = querystringify.parse(parsedBidUrl.query);
-
-            expect(parsedBidUrl.hostname).to.equal('cdn.yldbt.com');
-            expect(parsedBidUrl.pathname).to.equal('/js/yieldbot.intent.js');
-
-            var bidObjArr = JSON.parse(parsedBidUrlQueryString);
-            expect(bidObjArr).to.have.property('bids');
-            var bidObj1 = bidObjArr.bids[0];
-
-            expect(bidObj1.sizes).to.have.property('sizes').and.to.equal('300x250');
-            expect(bidObj1.psn).to.have.property('psn').and.to.equal('1234');
-            expect(bidObj1.psn).to.have.property('placementCode').and.to.equal('div-gpt-ad-1460505748561-0');
-
-
-            var bidObj2 = bidObjArr.bids[1];
-
-            expect(bidObj2).to.have.property('sizes').and.to.equal('728x90');
-            expect(bidObj2).to.have.property('psn').and.to.equal('1234');
-            expect(bidObj1.psn).to.have.property('placementCode').and.to.equal('div-gpt-ad-1460505661639-0');
-
-      });
-
-   });
-
-}); */
